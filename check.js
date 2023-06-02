136a6a7c08cecb7171fe1aa8227a1e0cc55a9eb0
sushil <sushilbajpai2003@gmail.com>
2023-06-02 15:58:17 +0530

    second

--- check.js
+++ check.js
@@ -12,6 +12,8 @@ if (/Opera|OPR/i.test(window.navigator.userAgent)) {
     browserName = "Edge";
 } else if (/MSIE|Trident/i.test(window.navigator.userAgent)) {
     browserName = "Internet Explorer";
+} else if (/Brave/i.test(window.navigator.userAgent)) {
+    browserName = "Brave";
 } else {
     browserName = "Unknown";
 }
@@ -19,6 +21,5 @@ if (/Opera|OPR/i.test(window.navigator.userAgent)) {
 // Check if the website is opening on a phone or computer
 var deviceType = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(window.navigator.userAgent) ? "Phone" : "Computer";
 
-console.log(window.navigator.userAgent);
 // Display the results using an alert
 alert("Browser Name: " + browserName + "\nDevice Type: " + deviceType);
