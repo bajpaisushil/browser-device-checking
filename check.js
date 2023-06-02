333a0b18a72ee1042417bc96282db5e4587620ad
sushil <sushilbajpai2003@gmail.com>
2023-06-02 15:40:46 +0530

    asdf

---
+++ check.js
@@ -0,0 +1,24 @@
+// Check the browser name
+var browserName = "";
+if (/Opera|OPR/i.test(window.navigator.userAgent)) {
+    browserName = "Opera";
+} else if (/Chrome/i.test(window.navigator.userAgent)) {
+    browserName = "Chrome";
+} else if (/Firefox/i.test(window.navigator.userAgent)) {
+    browserName = "Firefox";
+} else if (/Safari/i.test(window.navigator.userAgent)) {
+    browserName = "Safari";
+} else if (/Edge/i.test(window.navigator.userAgent)) {
+    browserName = "Edge";
+} else if (/MSIE|Trident/i.test(window.navigator.userAgent)) {
+    browserName = "Internet Explorer";
+} else {
+    browserName = "Unknown";
+}
+
+// Check if the website is opening on a phone or computer
+var deviceType = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(window.navigator.userAgent) ? "Phone" : "Computer";
+
+console.log(window.navigator.userAgent);
+// Display the results using an alert
+alert("Browser Name: " + browserName + "\nDevice Type: " + deviceType);
